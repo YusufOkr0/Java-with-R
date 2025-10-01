b53bd117f8f26f52a454ce6060549454f8d6323b
yusufokr0 <yusufokr0@gmail.com>
2025-10-01 09:46:32 +0300

    V3 java scheculed fixed

--- src/main/java/com/swe307/first_project/service/PlotGeneratorService.java
+++ src/main/java/com/swe307/first_project/service/PlotGeneratorService.java
@@ -1,24 +1,14 @@
 package com.swe307.first_project.service;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 
 @Service
 public class PlotGeneratorService {
 
-    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private final RCallerService rCallerService;
 
     private List<Double> allCsvData;
@@ -34,8 +24,7 @@ public class PlotGeneratorService {
         allCsvData = CSVLoader.loadCsvData();
     }
 
-    @Scheduled(fixedRate = 1000, initialDelay = 3000)
-    private void readTheDataAndPushToR() {
+    public void readTheDataAndPushToR() {
         if (allCsvData != null && csvIndex < allCsvData.size()) {
             double nextValue = allCsvData.get(csvIndex);
             csvIndex++;
