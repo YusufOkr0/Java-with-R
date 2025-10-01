5898e2bfcd89983ea46108cee7ce9e5d04e54e99
yusufokr0 <yusufokr0@gmail.com>
2025-10-01 09:28:50 +0300

    UI V3 fixed2

--- src/main/resources/script/plot.R
+++ src/main/resources/script/plot.R
@@ -1,5 +1,9 @@
-y_values <- rep(NA, 100)
-current_index <- 1
+library(lattice)
+
+# Global veri vektörü
+y_values <- numeric(100)  # 100 elemanlı vektör
+x_values <- 0:99          # x-axis 0-99
+current_index <- 1        # sıradaki konum, R 1-indexed
 
 update_plot_svg <- function(value, file_path="src/main/resources/static/plot.svg") {
   y_values[current_index] <<- value
@@ -8,14 +12,14 @@ update_plot_svg <- function(value, file_path="src/main/resources/static/plot.svg
   svg(filename = file_path, width = 9, height = 6)
 
   print(xyplot(
-    tail(y_values, 30) ~ tail(seq_along(y_values), 30),
+    y_values ~ x_values,
     type = "l",
     col.line = "brown",
     xlab = "Index",
     ylab = "Value",
-    main = "Dynamic Plot (Last 30 Points)",
+    main = "Dynamic Plot from Java Data",
     panel = function(...) {
-      panel.grid(h = -1, v = -1, col = "lightgray")
+      panel.grid(h = -1, v = -1, col = "lightgray")  # grid ekli
       panel.xyplot(...)
     }
   ))
